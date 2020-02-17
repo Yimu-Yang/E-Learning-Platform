3f865a64244cddb2430c563652098a59699104ed
Yimu Yang <yimuyang19911223@gmail.com>
2020-02-17 06:32:29 -0800

    refactor

--- Web Crawler/SQL/course.sql
+++ Web Crawler/SQL/course.sql
@@ -1,9 +1,10 @@
 show databases;
 use yimuyang;
-drop table course2;
 show tables;
+describe course3;
+drop table course3;
 
-CREATE TABLE course2 (
+CREATE TABLE course3 (
     courseName varchar(255) NOT NULL PRIMARY KEY,
     provider varchar(255),
     price varchar(255),
@@ -16,8 +17,18 @@ CREATE TABLE course2 (
     courseActualURL varchar(255)
 );
 
-describe course2;
-select * from course2;
-select * from course2 where imageURL="N/A" and videoURL="N/A";
-select count(*) from course2;
+select * from course3;
+select * from course3 where imageURL="N/A" and videoURL="N/A";
+select * from course3 where courseActualURL="N/A";
+select * from course3 where price NOT LIKE 'FREE%';
+select * from course3 where provider="Coursera";
+select * from course3 where provider="Udacity";
+select * from course3 where provider="Udemy";
+select * from course3 where provider="Treehouse";
+select * from course3 where provider="edX";
+select * from course3 where courseName="JavaScript Basics";
+select * from course3 where courseTalkURL="https://www.coursetalk.com/providers/edx/courses/customer-protection-rule-15c3-3";
+
+select count(*) from course3;
+
 
