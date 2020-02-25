d885ee2a490dab64343ef404db967557323bd4e2
Yimu Yang <yimuyang19911223@gmail.com>
2020-02-24 16:19:27 -0800

    refactor

--- Web Crawler/src/test/java/driver/DriverTest.java
+++ Web Crawler/src/test/java/driver/DriverTest.java
@@ -3,13 +3,20 @@ package driver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import java.io.IOException;
 
 import dao.CourseDao;
 
 public class DriverTest {
+
+    @Test
+    public void testPrint() {
+        String output = "test: mvn test";
+        assertEquals("test: mvn test", output, "Wrong Result");
+    }
 
 //    @Test
 //    public void testDriver() throws SQLException {
@@ -18,11 +25,6 @@ public class DriverTest {
 //        while (set.next()) {
 //            System.out.println(set.getString("courseName"));
 //        }
-//    }
-//
-//    @Test
-//    public void testPrint() {
-//        System.out.println("mvn tested!");
 //    }
 
 }
