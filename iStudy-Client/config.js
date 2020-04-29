cc0a89e0e67c040e9ef61c035e18ca436ae025f9
Yimu Yang <yimuyang19911223@gmail.com>
2020-04-29 03:05:53 -0700

    refactor

--- iStudy-Client/config.js
+++ iStudy-Client/config.js
@@ -1,5 +1,9 @@
+var host = window.location.hostname;
+
 module.exports = {
-    hostUrl: 'http://yimuyang1991.tk:8080',
+//    hostUrl: 'http://yimuyang1991.tk:8080',
+//    hostUrl: 'http://127.0.0.1:8080',
+    hostUrl: 'http://' + host + ':8080',
     clientUrl: 'http://localhost:3000',
 //    database: 'mongodb://localhost:27017/iStudy',
     secret: "$S$E$C$R$E$T$",
